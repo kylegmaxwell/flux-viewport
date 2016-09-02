566f81b4c024ae5ea34016dac32a5ef99e754bfe
Kyle Maxwell <kyle@flux.io>
2016-09-02 10:17:29 -0700

    Fix viewport for Safari - LIB3D-891
    Updating to a new version of flux-fetch removes const keywords, which
    are not supported by Safari. Also removed old qa folder which is
    replaced by the flux-viewport-qa repository.
    https://bitbucket.org/vannevartech/flux-viewport-qa
    
    How to see if this works:
     From genie/third_party/flux-json-to-three checkout https://cr.flux.io/#/c/13212/
     From genie/third_party/flux-viewport
     	checkout https://cr.flux.io/#/c/13213/
     	npm install
     	npm link ../flux-json-to-three
     	npm run build
     	npm start
     In Safari, navigate to http://localhost:8001/demo/basic.html
     You should see a viewport with a donut / torus.
    
    Change-Id: Ieba1e656747ab115374d182c0fb19c6f35bf7f14

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app = express();
 app.set('port', process.env.PORT||8001);
 
 app.use('/',express.static('./docs'));
+app.use('/demo',express.static('./demo'));
+app.use('/build',express.static('./build'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
